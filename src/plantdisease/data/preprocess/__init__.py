561a6b820e6126e6d60ce5329b48125ab3ca9968
KaveeshRamsarran <kaveesh.ramsarran@my.uwi.edu>
2026-02-09 19:10:01 -0400

    Grayscale Preprocessing

--- src/plantdisease/data/preprocess/__init__.py
+++ src/plantdisease/data/preprocess/__init__.py
@@ -1,1 +1,31 @@
 """Image preprocessing functions."""
+from .grayscale import (
+    to_grayscale,
+    batch_to_grayscale,
+    is_grayscale,
+    grayscale_with_alpha,
+)
+from .resize_standardize import (
+    resize_image,
+    standardize_image,
+    preprocess_image,
+    preprocess_directory,
+)
+from .denoise import denoise_median, denoise_bilateral, denoise_gaussian
+
+__all__ = [
+    # Grayscale
+    "to_grayscale",
+    "batch_to_grayscale",
+    "is_grayscale",
+    "grayscale_with_alpha",
+    # Resize/Standardize
+    "resize_image",
+    "standardize_image",
+    "preprocess_image",
+    "preprocess_directory",
+    # Denoise
+    "denoise_median",
+    "denoise_bilateral",
+    "denoise_gaussian",
+]
